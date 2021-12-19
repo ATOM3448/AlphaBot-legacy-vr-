c16137f121836112ed0ffac9ec34f486380f7252
ATOM3448 <60006878+ATOM3448@users.noreply.github.com>
2021-12-19 21:58:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'NzU3NTYzMDQ2MjU1MjYzNzU0.X2iNkQ.qIo2TIFEmiD1yzeaTx401GXXS3o',
+    'token': 'NzU3NTYzMDQ2MjU1MjYzNzU0.X2iNkQ.Fm0ORyMcVQ6XiGXPGAgaI2io7Lw',
     'bot': 'AlphaBOTv3',
     'id': 757563046255263754,
     'prefix': '!'
